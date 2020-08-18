0f86b8e7d43f6fb3205cbfdec01924af8311f517
Weida Zhong <weida.ustc@gmail.com>
2020-08-18 17:10:12 -0400

    Add missing to data

--- scripts/generate_data_samples.py
+++ scripts/generate_data_samples.py
@@ -1,10 +1,38 @@
 import argparse
 import os
+import random
 
 import numpy as np
 
+random.seed(0)
 
-def generate_graph_seq2seq_io_data(data, mask, x_offsets, y_offsets):
+
+def add_missing(mask: np.ndarray, missing_rate: int):
+    """
+    Add randomly missing to mask (set to 0)
+
+    Parameters
+    ----------
+    mask: np.ndarray
+        The original mask
+
+    missing_rate: int
+        The missing rate (percentage) to add to the mask.
+
+    Returns
+    -------
+    mask with added missing: np.ndarray
+        Have the same shape with input mask.
+    """
+    shape_backup = mask.shape
+    mask = mask.flatten()
+    # TODO: Deal with the existing missing in the mask
+    missing_index = random.sample(range(len(mask)), int(len(mask) * missing_rate / 100))
+    mask[missing_index] = 0
+    return mask.reshape(shape_backup)
+
+
+def generate_graph_seq2seq_io_data(data, mask, x_offsets, y_offsets, missing_rate: int):
     """
     Generate samples from data
 
@@ -25,6 +53,9 @@ def generate_graph_seq2seq_io_data(data, mask, x_offsets, y_offsets):
         The time interval offset for prediction.
         e.g., if prediction length is 12, it is [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]]
 
+    missing_rate: int
+        The missing rate to add to the mask
+
     Returns
     -------
     x: (num_samples, input_length, num_nodes, input_dim)
@@ -42,10 +73,13 @@ def generate_graph_seq2seq_io_data(data, mask, x_offsets, y_offsets):
     min_t = abs(min(x_offsets))
     max_t = abs(num_times - abs(max(y_offsets)))  # Exclusive
     for t in range(min_t, max_t):
-        x_t = data[t + x_offsets, ...]  # (12, 207, 2)
+        x_t = data[t + x_offsets, ...]  # (12, 200, 4)
         y_t = data[t + y_offsets, ...]  #
         mask_x_t = mask[t + x_offsets, ...]
         mask_y_t = mask[t + y_offsets, ...]
+        # add randomly missing to mask
+        mask_x_t = add_missing(mask_x_t, missing_rate)
+        mask_y_t = add_missing(mask_y_t, missing_rate)
         x.append(x_t)
         y.append(y_t)
         mask_x.append(mask_x_t)
@@ -57,7 +91,8 @@ def generate_graph_seq2seq_io_data(data, mask, x_offsets, y_offsets):
     return x, y, mask_x, mask_y
 
 
-def generate_train_val_test(source_data_filename: str, output_dir: str) -> None:
+def generate_train_val_test(source_data_filename: str, output_dir: str,
+                            input_length: int, predict_length: int, missing_rate: int) -> None:
     """
     Generate train, val, and test data.
 
@@ -69,6 +104,15 @@ def generate_train_val_test(source_data_filename: str, output_dir: str) -> None:
     output_dir: str
         The folder to hold the results.
 
+    input_length: int
+        The (time) length of input data
+
+    predict_length: int
+        The (time) length of predict
+
+    missing_rate: int
+        The missing rate (percentage)
+
     Returns
     -------
     None
@@ -78,14 +122,16 @@ def generate_train_val_test(source_data_filename: str, output_dir: str) -> None:
     mask = npz['mask']
 
     # 0 is the latest observed sample
-    x_offsets = np.arange(-11, 1, 1)
+    x_offsets = np.arange(-input_length + 1, 1, 1)
 
     # Predict the next one hour
-    y_offsets = np.arange(1, 13, 1)
+    y_offsets = np.arange(1, predict_length + 2, 1)
+
+    # TODO: scale??
 
     # x: (num_samples, input_length, num_nodes, input_dim)
     # y: (num_samples, output_length, num_nodes, output_dim)
-    x, y, mask_x, mask_y = generate_graph_seq2seq_io_data(data, mask, x_offsets=x_offsets, y_offsets=y_offsets)
+    x, y, mask_x, mask_y = generate_graph_seq2seq_io_data(data, mask, x_offsets, y_offsets, missing_rate)
 
     # x shape: (553, 12, 200, 4), y shape: (553, 12, 200, 4)
     # print("x shape: ", x.shape, ", y shape: ", y.shape)
@@ -116,13 +162,15 @@ def generate_train_val_test(source_data_filename: str, output_dir: str) -> None:
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir, exist_ok=True)
 
+    postfix = f"_{input_length}_{predict_length}_{missing_rate}.npz"
+
     for cat in ["train", "val", "test"]:
         _x, _y = locals()["x_" + cat], locals()["y_" + cat]
         _mask_x, _mask_y = locals()["mask_x_" + cat], locals()["mask_y_" + cat]
         # train x: (387, 12, 200, 4), y: (387, 12, 200, 4)
         print(cat, "x: ", _x.shape, "y:", _y.shape)
         np.savez_compressed(
-            os.path.join(args.output_dir, "%s.npz" % cat),
+            os.path.join(args.output_dir, cat + postfix),
             x=_x,
             y=_y,
             mask_x=_mask_x,
@@ -139,4 +187,11 @@ if __name__ == "__main__":
     parser.add_argument("--source_data_filename", type=str, default="data_raw/d07/data.npz",
                         help="Raw traffic readings.")
     args = parser.parse_args()
-    generate_train_val_test(args.source_data_filename, args.output_dir)
+    input_lengths = [12]
+    predict_lengths = [12]
+    missing_rates = [int(10 * i) for i in range(4, 5)]
+    for input_length in input_lengths:
+        for predict_length in predict_lengths:
+            for missing_rate in missing_rates:
+                generate_train_val_test(args.source_data_filename, args.output_dir,
+                                        input_length, predict_length, missing_rate)
